d78e56ae7f3a24ba7a8cb8fadbed3e51aa767490
benjaminzavala <benjamin@eadventist.net>
2025-12-01 17:11:15 -0500

    logs

--- resources/js/views/AdminReportsView.vue
+++ resources/js/views/AdminReportsView.vue
@@ -448,6 +448,7 @@ async function fetchCajaReport(download = false) {
             }
         } else {
             const data = await getCajaReport({ from_date: from, to_date: to });
+            console.log('caja report data', data);
             if (data instanceof Blob) {
                 cajaError.value = 'La respuesta del reporte no es válida.';
                 cajaData.value = null;
